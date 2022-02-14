705ce6db9a8fa108b08266caf07e1b008f9aa55b
Nioaes <whitney.a12221@gmail.com>
2022-02-14 17:45:23 -0500

    Added back previous code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {check, validationResult} = require('express-validator');
 
 require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Models = require('./model.js');
 
